032815385b648093c01ed3b0b4d2a92588bd0f30
Quentin Ikuta <49662235+qikuta@users.noreply.github.com>
2022-01-20 12:28:21 -0600

    chapter 10

--- p4eo10_3.py
+++ p4eo10_3.py
@@ -7,3 +7,30 @@
  languages. Compare your results with the tables at
  wikipedia.org/wiki/Letter_frequencies.
 """
+from string import ascii_lowercase
+
+fhand = open("mbox-short.txt")
+letterCount = dict()
+totalLetters = 0
+letterList = list()
+
+for line in fhand:
+    lowLine = line.lower()
+    for word in lowLine:
+        for letter in word:
+            if letter in ascii_lowercase:
+                totalLetters += 1
+                letterCount[letter] = letterCount.get(letter, 0) + 1
+
+
+for k, v in letterCount.items():
+    v = round(((v/totalLetters)*100), 2)
+    letterVals = (v, k)
+    letterList.append(letterVals)
+
+sortedLetterList = sorted(letterList, reverse=True)
+
+
+print("% Frequency / Letter:")
+for value in sortedLetterList:
+    print(value)
